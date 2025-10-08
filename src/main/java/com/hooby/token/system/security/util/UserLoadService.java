2c16546cbc5db81b1da4d7587d4a21633735c177
Hooby <hooby11105@naver.com>
2025-10-09 03:12:16 +0900

    feat: UserLoadService 정의

---
+++ src/main/java/com/hooby/token/system/security/util/UserLoadService.java
@@ -0,0 +1,25 @@
+package com.hooby.token.system.security.util;
+
+import com.hooby.token.domain.user.entity.User;
+import com.hooby.token.domain.user.repository.UserRepository;
+import com.hooby.token.system.security.model.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserLoadService {
+    private final UserRepository userRepository;
+
+    public Optional<UserPrincipal> loadUserById(Long userId) {
+        return userRepository.findById(userId)
+                .map(UserPrincipal::from);
+    }
+
+    public Optional<UserPrincipal> loadUserByUsername(String username) {
+        User user = userRepository.findByUsername(username);
+        return user != null ? Optional.of(UserPrincipal.from(user)) : Optional.empty();
+    }
+}
