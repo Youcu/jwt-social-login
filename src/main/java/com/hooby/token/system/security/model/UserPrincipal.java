80664d99ab16de9faf345da3f6e6f6dfbd24a83b
Hooby <hooby11105@naver.com>
2025-10-09 02:05:08 +0900

    feat: UserPrincipal 정의 (UserDetails)

---
+++ src/main/java/com/hooby/token/system/security/model/UserPrincipal.java
@@ -0,0 +1,62 @@
+package com.hooby.token.system.security.model;
+
+import com.hooby.token.domain.user.entity.Role;
+import com.hooby.token.domain.user.entity.User;
+import com.hooby.token.system.security.jwt.dto.JwtDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long userId; // OAuth2 User -> Nullable
+    private String username;
+    private String password; // OAuth2 User -> Nullable
+    private Role role;
+
+    public static UserPrincipal from(User u) { // Common User
+        return UserPrincipal.builder()
+                .userId(u.getId())
+                .username(u.getUsername())
+                .password(u.getPassword())
+                .role(u.getRole())
+                .build();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.toString())); // Override Method
+    }
+
+    @Override public String getUsername() { return username; }
+    @Override public String getPassword() { return password; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserPrincipal that)) return false;
+        return Objects.equals(this.userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.userId);
+    }
+}
+
