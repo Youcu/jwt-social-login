9d5b75f22d90c293171d79753ac211f9e760b7f3
Hooby <hooby11105@naver.com>
2025-10-10 06:54:34 +0900

    refactor: UserLoadService에 Transactional(readOnly=true) 적용

--- src/main/java/com/hooby/token/system/security/util/UserLoadService.java
+++ src/main/java/com/hooby/token/system/security/util/UserLoadService.java
@@ -5,11 +5,13 @@ import com.hooby.token.domain.user.repository.UserRepository;
 import com.hooby.token.system.security.model.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserLoadService {
     private final UserRepository userRepository;
 
